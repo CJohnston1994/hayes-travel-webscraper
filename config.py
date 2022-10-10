ad202924628a2171b28764e8e13617ff2d0bf94a
TheGlink <c.johnston1994@hotmail.co.uk>
2022-10-10 21:37:53 +0100

    Complete

--- config.py
+++ config.py
@@ -1,17 +1,23 @@
 import os
 
+'''test'''
+S3_ACCESS_KEY='AKIAZM456EJJJOXJ7H6V'
+S3_SECRET_KEY='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'
+HOST='hayes-travel-scraper.c3deqrfbkahl.us-east-1.rds.amazonaws.com'
+PASSWORD='Nicole2005!'
+
 '''
 AWS s3_client details
 '''
-S3_ACCESS = os.environ.get('S3_ACCESS_KEY')
-S3_SECRET = os.environ.get('S3_SECRET_KEY')
+#S3_ACCESS = os.environ.get('S3_ACCESS_KEY')
+#S3_SECRET = os.environ.get('S3_SECRET_KEY')
 S3_BUCKET = 'hayes-travel-web-scraper'
 
 '''
 SQLalchemy database engine details
 '''
-HOST = os.environ['HOST']
-PASSWORD = os.environ["PASSWORD"]
+#HOST = os.environ['HOST']
+#PASSWORD = os.environ["PASSWORD"]
 DATABASE_TYPE = 'postgresql'
 DBAPI = 'psycopg2'
 USER = 'postgres'
